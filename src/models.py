633eae9b0cc5c3eb80f1c46c5b6ba87fc0be91ea
Joshua <marichaljoshua@gmail.com>
2021-04-20 23:05:20 +0000

    Instragram modal

--- src/models.py
+++ src/models.py
@@ -8,23 +8,37 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True) 
+    firstName = Column(String(50))
+    lastName = Column(String(50))
+    email = Column(String(80), nullable=False)
+    picture = Column(String())
+    user = relationship('Post', back_populates="user")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    date = Column(String(25))
+    likes = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id')) 
+    media = relationship("Media", uselist=False, back_populates="post")
+    comment = relationship("Comment", uselist=False, back_populates="post")
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String())
+    description = Column(String(300), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    commentText = Column(String(300), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
 
     def to_dict(self):
         return {}
